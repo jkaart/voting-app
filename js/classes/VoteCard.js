ce650c2660b401ec8505e8de97e18a85452ed5cd
Jani Kaartinen <jkaart@gmail.com>
2024-10-30 14:36:08 +0200

    votecard fix and percentage round

--- js/classes/VoteCard.js
+++ js/classes/VoteCard.js
@@ -9,11 +9,11 @@ const generateCardContainer = (voteData) => {
     cardContainer.classList.add('col');
     cardContainer.innerHTML = `
         <div class="bg-light text-dark card">
+            <div class="card-header d-flex justify-content-between">
+                <h5 class="card-title text-break">${voteData.title}</h5>
+                <h5 class="fs-6 text-nowrap" id="vote${voteData.id}TotalVotes">Total votes: ${voteData.totalVoteCount}</h5>
+            </div>
             <div class="card-body">
-                <div>
-                    <h5 class="float-end fs-6" id="vote${voteData.id}TotalVotes">Total votes: ${voteData.totalVoteCount}</h5>
-                    <h5 class="card-title">${voteData.title}</h5>
-                </div>
                 <div class="my-2">
                     ${voteData.description}
                 </div>
@@ -47,12 +47,14 @@ class VoteCard {
         this.cardContainer = generateCardContainer(this.voteData);
         this.cardContainer.children[0].addEventListener('click', () => { openViewVoteModalEventHandler(this.voteData) });
         this.voteContainer.appendChild(this.cardContainer);
-        this.cardBody = this.cardContainer.children[0].children[0];
-        this.cardFooter = this.cardContainer.children[0].children[1];
-        this.voteBody = this.cardBody.children[2];
-        this.voteCounterSpans = this.voteBody.querySelectorAll('span.voteCounter');
-        this.voteProgressDivs = this.voteBody.querySelectorAll('div.voteProgress');
-        this.totalVotesH5 = this.cardBody.children[0].children[0];
+        this.cardHeader = this.cardContainer.children[0].children[0];
+        this.cardBody = this.cardContainer.children[0].children[1];
+        console.log(this.cardBody)
+        this.cardFooter = this.cardContainer.children[0].children[2];
+        this.voteCounterSpans = this.cardBody.querySelectorAll('span.voteCounter');
+        this.voteProgressDivs = this.cardBody.querySelectorAll('div.voteProgress');
+        this.totalVotesH5 = this.cardHeader.children[1];
+        this.updateProgressBars();
     }
 
     get voteData() {
--- js/functions/percentage.js
+++ js/functions/percentage.js
@@ -1,5 +1,5 @@
 const calcPercentage = (value, total) => {
-    return (100 * value) / total;
+    return Math.round((100 * value) / total);
 
 }
 export { calcPercentage }
